b0ff71aad682121b048eef7052285ba8960d2930
Clemens Zangl <clemens.zangl@woven-planet.global>
2022-01-26 11:20:47 +0100

    Adds Helper Functions to De/Encode fixed-width Integers
    - These helper functions are useful to make the generated code more
      readable.

--- ztype/bits_encode.go
+++ ztype/bits_encode.go
@@ -34,3 +34,43 @@ func WriteSignedBits(w *bitio.CountWriter, value int64, bits uint8) error {
 	var v uint64 = (1 << bits) | uint64(value+(1<<bits))
 	return w.WriteBits(v, bits)
 }
+
+// WriteUint8 writes an unsigned 8-bit integer.
+func WriteUint8(w *bitio.CountWriter, value uint8) error {
+	return WriteUnsignedBits(w, uint64(value), 8)
+}
+
+// WriteUint16 writes an unsigned 16-bit integer.
+func WriteUint16(w *bitio.CountWriter, value uint16) error {
+	return WriteUnsignedBits(w, uint64(value), 16)
+}
+
+// WriteUint32 writes an unsigned 32-bit integer.
+func WriteUint32(w *bitio.CountWriter, value uint32) error {
+	return WriteUnsignedBits(w, uint64(value), 32)
+}
+
+// WriteUint64 writes an unsigned 64-bit integer.
+func WriteUint64(w *bitio.CountWriter, value uint64) error {
+	return WriteUnsignedBits(w, uint64(value), 64)
+}
+
+// WriteInt8 writes a signed 8-bit integer.
+func WriteInt8(w *bitio.CountWriter, value int8) error {
+	return WriteSignedBits(w, int64(value), 8)
+}
+
+// WriteInt16 writes a signed 16-bit integer.
+func WriteInt16(w *bitio.CountWriter, value int16) error {
+	return WriteSignedBits(w, int64(value), 16)
+}
+
+// WriteInt32 writes a signed 32-bit integer.
+func WriteInt32(w *bitio.CountWriter, value int32) error {
+	return WriteSignedBits(w, int64(value), 32)
+}
+
+// WriteInt64 writes a signed 64-bit integer.
+func WriteInt64(w *bitio.CountWriter, value int64) error {
+	return WriteSignedBits(w, int64(value), 64)
+}
