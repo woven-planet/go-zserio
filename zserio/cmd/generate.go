77bb2ff6338809f9aa27aa0c0d21c4c2c6df8867
Clemens Zangl <clemens.zangl@woven-planet.global>
2022-01-26 15:22:40 +0100

    Add a CLI Command "generate"

---
+++ zserio/cmd/generate.go
@@ -0,0 +1,46 @@
+package cmd
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/spf13/cobra"
+	"github.com/woven-planet/go-zserio/internal/generator"
+	"github.com/woven-planet/go-zserio/internal/model"
+)
+
+var (
+	outputDirectory string
+	topLevelPackage string
+)
+
+var generateCmd = &cobra.Command{
+	Use:   "generate",
+	Short: "Generates Go files for the source zserio files",
+	Args:  cobra.ExactArgs(1),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		m, err := model.FromFilesystem(args[0])
+		if err != nil {
+			fmt.Fprintf(cmd.ErrOrStderr(), "Parse error: %v", err)
+			return err
+		}
+
+		if err = generator.Generate(m, strings.TrimSpace(outputDirectory), &generator.Options{
+			RootPackage: strings.TrimSpace(topLevelPackage),
+		}); err != nil {
+			fmt.Fprintf(cmd.ErrOrStderr(), "Code generation error: %v", err)
+			return err
+		}
+		return nil
+	},
+}
+
+func init() {
+	generateCmd.Flags().StringVarP(&outputDirectory, "out", "o", "", "Output directory to generate Go files to")
+	generateCmd.MarkFlagRequired("out")
+
+	generateCmd.Flags().StringVarP(&topLevelPackage, "rootpackage", "r", "", "Name of the root package package")
+	generateCmd.MarkFlagRequired("rootpackage")
+
+	rootCmd.AddCommand(generateCmd)
+}
