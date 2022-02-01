10ae5a51179d4625eb5e77464a4e9b45fd598ae7
Ignas Anikevicius <ignas.anikevicius@woven-planet.global>
2022-02-01 10:33:34 +0200

    ztype: add clone for array to implement ZserioType

---
+++ ztype/array_test.go
@@ -0,0 +1,9 @@
+package ztype_test
+
+import (
+	"github.com/woven-planet/go-zserio/interface"
+	"github.com/woven-planet/go-zserio/ztype"
+)
+
+// This is a compile-time check to see that we are implementing the interface.
+var _ zserio.ZserioType = (*ztype.Array[int64, *ztype.VarIntArrayTraits])(nil)
