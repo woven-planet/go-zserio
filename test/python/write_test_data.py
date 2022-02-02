5454b4293a9a5c16d568bac8f594e3627cb15fe2
Ignas Anikevicius <ignas.anikevicius@woven-planet.global>
2022-02-02 08:34:27 +0200

    generate the binary using a genrule

--- test/python/write_test_data.py
+++ test/python/write_test_data.py
@@ -1,10 +1,9 @@
-from re import S
 import os
-import zserio
 import sys
-sys.path.append(os.path.dirname(__file__))
 
-from testdata.reference_modules.core.instantiations.instantiated_template_struct import (
+import zserio
+
+from test.python.testdata.reference_modules.core.instantiations.instantiated_template_struct import (
     InstantiatedTemplateStruct,
 )
 from testdata.reference_modules.testobject1.testobject.test_object import TestObject
@@ -12,40 +11,46 @@ from testdata.reference_modules.core.types.value_wrapper import ValueWrapper
 from testdata.reference_modules.core.types.color import Color
 from testdata.reference_modules.core.types.city_attributes import CityAttributes
 
-if __name__ == "__main__":
-    # Create a dummy object with some random values
-    test_data = TestObject()
-    test_data.parameter1 = 7
+def _new_test_object():
+    """Create a dummy object with some random values.
+    """
+    d = TestObject()
+    d.parameter1 = 7
 
-    struct1_field = ValueWrapper(test_data.parameter1)
+    struct1_field = ValueWrapper(d.parameter1)
     struct1_field.value = 72
     struct1_field.other_value = 121
     struct1_field.enum_value = Color.BLUE
     struct1_field.description = "test data"
 
-    struct1 = InstantiatedTemplateStruct(test_data.parameter1)
+    struct1 = InstantiatedTemplateStruct(d.parameter1)
     struct1.field = struct1_field
-    test_data.struct1 = struct1
-    test_data.color1 = Color.RED
+    d.struct1 = struct1
+    d.color1 = Color.RED
 
-    test_data.parameter2 = 12
+    d.parameter2 = 12
 
     for i in range(22):
-        current_struct_field = ValueWrapper(test_data.parameter2)
+        current_struct_field = ValueWrapper(d.parameter2)
         current_struct_field.value = i + 100
         current_struct_field.other_value = i
         current_struct_field.description = "some dummy description"
         current_struct_field.enum_value = Color.BLACK
-        current_struct = InstantiatedTemplateStruct(test_data.parameter2)
+        current_struct = InstantiatedTemplateStruct(d.parameter2)
         current_struct.field = current_struct_field
-        test_data.struct_array.append(current_struct)
+        d.struct_array.append(current_struct)
 
     for i in range(100):
-        test_data.bitmask_array.append(CityAttributes.from_value(2))
+        d.bitmask_array.append(CityAttributes.from_value(2))
 
+    return d
+
+def main():
+    out = sys.argv[1]
     # write the testdata
-    os.mkdir("bin")
-    byte_data = zserio.serialize_to_bytes(test_data)
-    handle = open("bin/testdata.bin", "wb")
-    handle.write(byte_data)
-    handle.close()
+    byte_data = zserio.serialize_to_bytes(_new_test_object())
+    with open(out, "wb") as f:
+        f.write(byte_data)
+
+if __name__ == "__main__":
+    main()
