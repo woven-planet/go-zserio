fc57eb57fc226c386916b29a988aad11f7f4e034
Ignas Anikevicius <ignas.anikevicius@woven-planet.global>
2022-02-09 13:47:48 +0200

    use interfaces for serde of floats

--- ztype/float_encode.go
+++ ztype/float_encode.go
@@ -1,24 +1,24 @@
 package ztype
 
 import (
+	"io"
 	"encoding/binary"
 
-	"github.com/icza/bitio"
 	"github.com/x448/float16"
 )
 
 // WriteFloat16 writes a float value in half precision (FP16) format.
-func WriteFloat16(w *bitio.CountWriter, v float32) error {
+func WriteFloat16(w io.Writer, v float32) error {
 	f := float16.Fromfloat32(v).Bits()
 	return binary.Write(w, binary.BigEndian, f)
 }
 
 // WriteFloat32 writes a float32 to the bitstream.
-func WriteFloat32(w *bitio.CountWriter, v float32) error {
+func WriteFloat32(w io.Writer, v float32) error {
 	return binary.Write(w, binary.BigEndian, v)
 }
 
 // WriteFloat64 writes a float64 to the bitstream.
-func WriteFloat64(w *bitio.CountWriter, v float64) error {
+func WriteFloat64(w io.Writer, v float64) error {
 	return binary.Write(w, binary.BigEndian, v)
 }
