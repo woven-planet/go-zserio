c6376d4f170abc9ecf1d6e8028e6bf923dbe362f
Ignas Anikevicius <ignas.anikevicius@woven-planet.global>
2022-02-09 14:07:28 +0200

    introduce BitWriter

--- ztype/bits_encode.go
+++ ztype/bits_encode.go
@@ -1,17 +1,17 @@
 package ztype
 
-import (
-	"errors"
+import "errors"
 
-	"github.com/icza/bitio"
-)
+type BitWriter interface {
+	WriteBits(r uint64, n uint8) error
+}
 
 // ErrValueOutOfBitRange is returned when you try to write a bit field type
 // with a value that is larger than the number of bits allows for.
 var ErrValueOutOfBitRange = errors.New("value is out of range for the given number of bits")
 
 // WriteUnsignedBits writes an unsigned number with a given number of bits
-func WriteUnsignedBits(w *bitio.CountWriter, value uint64, bits uint8) error {
+func WriteUnsignedBits(w BitWriter, value uint64, bits uint8) error {
 	if value > ((1 << bits) - 1) {
 		return ErrValueOutOfBitRange
 	}
@@ -19,7 +19,7 @@ func WriteUnsignedBits(w *bitio.CountWriter, value uint64, bits uint8) error {
 }
 
 // WriteSignedBits writes an signed number with a given number of bits
-func WriteSignedBits(w *bitio.CountWriter, value int64, bits uint8) error {
+func WriteSignedBits(w BitWriter, value int64, bits uint8) error {
 	if bits > 64 {
 		panic("can not read more than 64 bits")
 	}
@@ -38,41 +38,41 @@ func WriteSignedBits(w *bitio.CountWriter, value int64, bits uint8) error {
 }
 
 // WriteUint8 writes an unsigned 8-bit integer.
-func WriteUint8(w *bitio.CountWriter, value uint8) error {
+func WriteUint8(w BitWriter, value uint8) error {
 	return WriteUnsignedBits(w, uint64(value), 8)
 }
 
 // WriteUint16 writes an unsigned 16-bit integer.
-func WriteUint16(w *bitio.CountWriter, value uint16) error {
+func WriteUint16(w BitWriter, value uint16) error {
 	return WriteUnsignedBits(w, uint64(value), 16)
 }
 
 // WriteUint32 writes an unsigned 32-bit integer.
-func WriteUint32(w *bitio.CountWriter, value uint32) error {
+func WriteUint32(w BitWriter, value uint32) error {
 	return WriteUnsignedBits(w, uint64(value), 32)
 }
 
 // WriteUint64 writes an unsigned 64-bit integer.
-func WriteUint64(w *bitio.CountWriter, value uint64) error {
+func WriteUint64(w BitWriter, value uint64) error {
 	return WriteUnsignedBits(w, uint64(value), 64)
 }
 
 // WriteInt8 writes a signed 8-bit integer.
-func WriteInt8(w *bitio.CountWriter, value int8) error {
+func WriteInt8(w BitWriter, value int8) error {
 	return WriteSignedBits(w, int64(value), 8)
 }
 
 // WriteInt16 writes a signed 16-bit integer.
-func WriteInt16(w *bitio.CountWriter, value int16) error {
+func WriteInt16(w BitWriter, value int16) error {
 	return WriteSignedBits(w, int64(value), 16)
 }
 
 // WriteInt32 writes a signed 32-bit integer.
-func WriteInt32(w *bitio.CountWriter, value int32) error {
+func WriteInt32(w BitWriter, value int32) error {
 	return WriteSignedBits(w, int64(value), 32)
 }
 
 // WriteInt64 writes a signed 64-bit integer.
-func WriteInt64(w *bitio.CountWriter, value int64) error {
+func WriteInt64(w BitWriter, value int64) error {
 	return WriteSignedBits(w, int64(value), 64)
 }
