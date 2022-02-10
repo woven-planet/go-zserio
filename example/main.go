ef0bd766fe935d2273b9558b315410efb529912f
Ignas Anikevicius <ignas.anikevicius@woven-planet.global>
2022-02-10 12:42:40 +0200

    adapt the example code

--- example/main.go
+++ example/main.go
@@ -4,17 +4,16 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/icza/bitio"
+	"github.com/woven-planet/go-zserio/ztype"
 	"myprojects.home/zserio-example/contacts"
 )
 
 func main() {
+	writer := ztype.NewWriter(os.Stdout)
+	defer writer.Close()
+
 	address := contacts.Address{Street: "Mainstreet"}
-	writer := bitio.NewCountWriter(os.Stdout)
 	if err := address.MarshalZserio(writer); err != nil {
 		panic(fmt.Sprintf("error serializing address: %v", err))
-	}
-	if err := writer.Close(); err != nil {
-		panic(fmt.Sprintf("error closing writer: %v", err))
 	}
 }
