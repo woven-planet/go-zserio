cebb06a09ab2e940039d91e2b01b008913b68a1b
Ignas Anikevicius <ignas.anikevicius@woven-planet.global>
2022-02-14 16:20:19 +0200

    cleanup

--- internal/parser/constants.go
+++ internal/parser/constants.go
@@ -1,13 +1,8 @@
 package parser
 
-import "github.com/antlr/antlr4/runtime/Go/antlr"
-
 const (
 	// UnevaluatableExpressionType is as an expression type which cannot be
 	// evaluated by itself. An example is the right part of a dot expression:
 	// <type>.<value>, where <value> cannot be evaluated without the type.
 	UnevaluatableExpressionType = 0xFFFFFF
 )
-
-// force the dependency resolution
-var _ any = (antlr.RecognitionException)(nil)
